411fbebcb46ca490015f7f159714f3021daf6c6f
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2023-03-12 22:40:57 -0500

    resumeDatajs, serviceWorkerjs, and two pictures

---
+++ src/resumeData.js
@@ -0,0 +1,91 @@
+
+let resumeData = {
+"imagebaseurl":"https://uhdznataly.github.io/",
+"name": "Nataly Rodriguez",
+"role": "Full Stack Developer in training",
+"linkedinId":"https://www.linkedin.com/in/natrod/",
+"roleDescription": "I like to...",
+"socialLinks":[
+    {
+      "name":"linkedin",
+      "url":"https://www.linkedin.com/in/natrod/",
+      "className":"fa fa-linkedin"
+    },
+    {
+      "name":"github",
+      "url":"http://github.com/uhdznataly",
+      "className":"fa fa-github"
+    }
+    
+  ],
+"aboutme":"I just finished my education in coding at the University of Texas Coding Bootcamp. ",
+"address":"Austin, TX",
+"website":"https://uhdznataly.github.io",
+"education":[
+  {
+    "UniversityName":"University of Houston",
+    "specialization":"Education",
+    "MonthOfPassing":"May",
+    "YearOfPassing":"2017"
+  },
+  {
+    "UniversityName":"University of Texas Coding Bootcamp",
+    "specialization":"Full Stack Web Development ",
+    "MonthOfPassing":"March",
+    "YearOfPassing":"2023"
+  }
+],
+"work":[
+  {
+    "CompanyName":"Manor Middle School",
+    "specialization":"Math Teacher/LPAC Representative",
+    "MonthOfLeaving":"August 2021",
+    "YearOfLeaving":"present"
+  },
+  {
+    "CompanyName":"Stevenson Middle School",
+    "specialization":"Math Teacher",
+    "MonthOfLeaving":"August 2018",
+    "YearOfLeaving":"August 2021"
+  }
+],
+"skillsDescription":"Your skills here",
+"skills":[
+  {
+    "skillname":"HTML5"
+  },
+  {
+    "skillname":"CSS"
+  },
+  {
+    "skillname":"Reactjs"
+  },
+  {
+    "skillname":"JavaScript"
+  },
+  {
+    "skillname":"Nodejs"
+  }
+],
+"portfolio":[
+  {
+    "name":"Cinemate",
+    "description":"",
+    "imgurl":"images/portfolio/Cinemate.png"
+  },
+  {
+    "name":"Operation Wellness",
+    "description":"",
+    "imgurl":"images/portfolio/OperationWellness.png"
+  }
+
+],
+"testimonials":[
+  {
+    "description":"This is a sample testimonial",
+    "name":"Random Person"
+  }
+]
+};
+
+export default resumeData
